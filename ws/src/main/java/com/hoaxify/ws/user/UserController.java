3e4a56af1c6c08a60abeca86f64645e01307e1bc
Eren Nokta <eren.nkta@gmail.com>
2023-08-07 12:17:52 +0300

    User Update Commited
    Kullanıcı güncellemek için bir api oluşturduk. Bu api'de bir dto objesi döndük ki istediğimiz fieldları filtreleyebilelim. Daha sonra spring security ile authorize olmayan ve kendine ait olmayan kullanıcıların  başkalarının bilgilerini değiştirmesini engelledik

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,15 +5,21 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import com.hoaxify.ws.user.vm.UserVM;
 
 @RestController
@@ -44,6 +50,12 @@ public class UserController {
 		User user =userService.getByUsername(username); //Bu service'i başka yerlerde de kullanabiliriz ondan VM kullanmasak daha iyi olur
 		return new UserVM(user);
 		
+	}
+	@PutMapping("users/{username}")
+	@PreAuthorize("#username == principal.username") // SpEl ile yazacağız
+	UserVM updateUser(@RequestBody UserUpdateVM updatedUser,@PathVariable String username ) {
+		User user = userService.updateUser(username,updatedUser);
+		return new UserVM(user);
 	}
 
 
