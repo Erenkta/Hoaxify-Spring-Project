c25669fae6fc8c42eafadbdd486945a7d9b75dee
Eren Nokta <eren.nkta@gmail.com>
2023-08-08 15:40:04 +0300

    Hoax Validation Commited
    Hoax için gerekli olan validation'lar halloldu

--- ws/src/main/java/com/hoaxify/ws/hoax/Hoax.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/Hoax.java
@@ -3,11 +3,13 @@ package com.hoaxify.ws.hoax;
 import java.security.Timestamp;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -19,6 +21,8 @@ public class Hoax {
 	@GeneratedValue
 	private long id;
 	
+	@Size(min=5,max=1000)
+	@Column(length = 1000) //eğer string ise varsayılan 255 bunu 1000 e çektik ki max değer de düzgün çalışsın
 	private String content;	
 	
 	@Temporal(TemporalType.DATE) // sadece tarih tutsun istedik 
